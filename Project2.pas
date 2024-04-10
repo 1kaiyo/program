2be165fb52057ad0dd6aa597096829c5645f83db
1kaiyo <163761258+1kaiyo@users.noreply.github.com>
2024-04-10 17:58:07 +0500

    Add files via upload

---
+++ Project2.pas
@@ -0,0 +1,21 @@
+﻿program CreateAndWriteToFile;
+var
+  myFile: Text;
+  i, randomNumber: integer;
+
+begin
+  Assign(myFile, 'myFile.txt');
+  Rewrite(myFile);
+
+Randomize;
+  for i := 1 to 20 do
+  begin
+    randomNumber := Random(1000);
+    writeln(myFile, randomNumber, ' ');
+  end;
+
+
+  Close(myFile);
+
+  writeln('Текст успешно записан в файл.');
+end.
